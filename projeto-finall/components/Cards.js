477b3f021952cc539f759befe185673e9c05ec12
Gabriel Perozim <gabrielperozim7@gmail.com>
2022-11-16 17:37:34 -0300

    Alterações

--- projeto-finall/components/Cards.js
+++ projeto-finall/components/Cards.js
@@ -1,15 +1,25 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import axios from 'axios';
+import ActiveLink from "./ActiveLink.tsx";
 
 export default function Example() {
   const urlAPI = "http://localhost:5172/api/servico";
   const initialState = {
-    produto: { id: 0, name: "", desc: "", preco: 0, img: ""},
+    produto: { id: 0, name: "", desc: "", preco: 0, img: "", },
     lista: [],
+    menus: [
+      {
+        path: "/posts/cod/cod"
+      },
+      {
+        path: "/posts/destiny/destiny"
+      }
+    ],
   };
 
   const [produto, setProduto] = useState(initialState.produto);
   const [lista, setLista] = useState(initialState.lista);
+  const [menus, setMenu] = useState(initialState.menus);
 
   const dataFromAPI = async () => {
     return await axios(urlAPI)
@@ -23,8 +33,8 @@ export default function Example() {
       .catch((error) => console.log(error));
   }, [produto]);
 
-  console.log(`lista: ${lista}`)
-  console.log(`produtos: ${produto}`)
+
+
 
   return (
     <div className="bg-white">
@@ -34,31 +44,34 @@ export default function Example() {
         </h2>
 
         <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-10 sm:grid-cols-2 lg:grid-cols-5 xl:gap-x-8">
-          {lista.map((product) => (
-            <div key={product.id} className="group relative">
-              <div className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
-                <img
-                  src={product.img}
-                  alt={product.imageAlt}
-                  className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-                />
-              </div>
-              <div className="mt-4 flex justify-between">
-                <div>
-                  <h3 className="text-sm text-gray-700">
-                    <a href={product.href}>
-                      <span aria-hidden="true" className="absolute inset-0" />
-                      {product.name}
-                    </a>
-                  </h3>
-                  <p className="mt-1 text-sm text-gray-500">{product.color}</p>
-                </div>
-                <p className="text-sm font-medium text-gray-900">
-                  {product.preco}
-                </p>
-              </div>
-            </div>
-          ))}
+              {menus.map((nav, product) => (
+                <>
+                  <ActiveLink href={nav.path}>
+                    <div key={product.id} className="group relative">
+                      <div className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
+                        <img
+                          src={product.img}
+                          className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                        />
+                      </div>
+                      <div className="mt-4 flex flex-col items-start justify-center">
+                        <div>
+                          <h3 className="text-sm text-gray-700">
+                            <a href={product.href}>
+                              <span aria-hidden="true" className="absolute inset-0" />
+                              {product.name}
+                            </a>
+                          </h3>
+                          <p className="mt-1 text-sm text-gray-500">{product.color}</p>
+                        </div>
+                        <p className="text-sm font-medium text-gray-900">
+                          R$: {product.preco},00
+                        </p>
+                      </div>
+                    </div>
+                  </ActiveLink>
+                </>
+              ))}
         </div>
       </div>
     </div>
--- projeto-finall/pages/posts/cod/[cod].js
+++ projeto-finall/pages/posts/cod/[cod].js
@@ -1,16 +1,10 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { StarIcon } from "@heroicons/react/20/solid";
 import { RadioGroup } from "@headlessui/react";
 import Navbar from "../../../components/Navbar";
+import axios from "axios";
 
 const product = {
-  name: "Basic Tee 6-Pack",
-  price: "$192",
-  href: "#",
-  breadcrumbs: [
-    { id: 1, name: "Men", href: "#" },
-    { id: 2, name: "Clothing", href: "#" },
-  ],
   images: [
     {
       src: "../../warzone.jpg",
@@ -34,82 +28,46 @@ const product = {
     { name: "Gray", class: "bg-gray-200", selectedClass: "ring-gray-400" },
     { name: "Black", class: "bg-gray-900", selectedClass: "ring-gray-900" },
   ],
-  sizes: [
-    { name: "XXS", inStock: false },
-    { name: "XS", inStock: true },
-    { name: "S", inStock: true },
-    { name: "M", inStock: true },
-    { name: "L", inStock: true },
-    { name: "XL", inStock: true },
-    { name: "2XL", inStock: true },
-    { name: "3XL", inStock: true },
-  ],
-  description:
-    "Destiny 2 precede o videogame original, Destiny, um jogo de tiro em primeira pessoa online com plataforma multiplayer. Ele tem opções de jogador contra ambiente (PvE) e jogador contra jogador (PvP) que permitem ao usuário escolher se quer lutar contra o jogo ou contra uma pessoa real.",
   highlights: [
     "Hand cut and sewn locally",
     "Dyed with our proprietary colors",
     "Pre-washed & pre-shrunk",
     "Ultra-soft 100% cotton",
   ],
-  details:
-    'The 6-Pack includes two black, two white, and two heather gray Basic Tees. Sign up for our subscription service and be the first to get new, exciting colors, like our upcoming "Charcoal Gray" limited release.',
 };
-const reviews = { href: "#", average: 4, totalCount: 117 };
+const reviews = { href: "#", average: 5, totalCount: 117 };
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
 export default function Cod() {
+  const urlAPI = "http://localhost:5172/api/servico";
+  const initialState = {
+    produto: { id: 0, name: "", descricao: "", preco: 0, img: "" },
+    lista: [],
+  };
+
+  const [produto, setProduto] = useState(initialState.produto[1]);
+  const [lista, setLista] = useState(initialState.lista[1]);
   const [selectedColor, setSelectedColor] = useState(product.colors[0]);
-  const [selectedSize, setSelectedSize] = useState(product.sizes[2]);
+
+  const dataFromAPI = async () => {
+    return await axios(urlAPI)
+      .then((resp) => resp.data)
+      .catch((err) => err);
+  };
+
+  useEffect(() => {
+    dataFromAPI()
+      .then(setLista)
+      .catch((error) => console.log(error));
+  }, [produto]);
 
   return (
     <div className="bg-white">
       <Navbar />
       <div className="pt-6">
-        <nav aria-label="Breadcrumb">
-          <ol
-            role="list"
-            className="mx-auto flex max-w-2xl items-center space-x-2 px-4 sm:px-6 lg:max-w-7xl lg:px-8"
-          >
-            {product.breadcrumbs.map((breadcrumb) => (
-              <li key={breadcrumb.id}>
-                <div className="flex items-center">
-                  <a
-                    href={breadcrumb.href}
-                    className="mr-2 text-sm font-medium text-gray-900"
-                  >
-                    {breadcrumb.name}
-                  </a>
-                  <svg
-                    width={16}
-                    height={20}
-                    viewBox="0 0 16 20"
-                    fill="currentColor"
-                    xmlns="http://www.w3.org/2000/svg"
-                    aria-hidden="true"
-                    className="h-5 w-4 text-gray-300"
-                  >
-                    <path d="M5.697 4.34L8.98 16.532h1.327L7.025 4.341H5.697z" />
-                  </svg>
-                </div>
-              </li>
-            ))}
-            <li className="text-sm">
-              <a
-                href={product.href}
-                aria-current="page"
-                className="font-medium text-gray-500 hover:text-gray-600"
-              >
-                {product.name}
-              </a>
-            </li>
-          </ol>
-        </nav>
-
-        {/* Image gallery */}
         <div className="mx-auto mt-6 max-w-2xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-x-8 lg:px-8">
           <div className="aspect-w-3 aspect-h-4 hidden overflow-hidden rounded-lg lg:block">
             <img
@@ -230,105 +188,22 @@ export default function Cod() {
                   </div>
                 </RadioGroup>
               </div>
-
-              {/* Sizes */}
-              <div className="mt-10">
-                <div className="flex items-center justify-between">
-                  <h3 className="text-sm font-medium text-gray-900">Size</h3>
-                  <a
-                    href="#"
-                    className="text-sm font-medium text-indigo-600 hover:text-indigo-500"
-                  >
-                    Size guide
-                  </a>
-                </div>
-
-                <RadioGroup
-                  value={selectedSize}
-                  onChange={setSelectedSize}
-                  className="mt-4"
-                >
-                  <RadioGroup.Label className="sr-only">
-                    {" "}
-                    Choose a size{" "}
-                  </RadioGroup.Label>
-                  <div className="grid grid-cols-4 gap-4 sm:grid-cols-8 lg:grid-cols-4">
-                    {product.sizes.map((size) => (
-                      <RadioGroup.Option
-                        key={size.name}
-                        value={size}
-                        disabled={!size.inStock}
-                        className={({ active }) =>
-                          classNames(
-                            size.inStock
-                              ? "bg-white shadow-sm text-gray-900 cursor-pointer"
-                              : "bg-gray-50 text-gray-200 cursor-not-allowed",
-                            active ? "ring-2 ring-indigo-500" : "",
-                            "group relative border rounded-md py-3 px-4 flex items-center justify-center text-sm font-medium uppercase hover:bg-gray-50 focus:outline-none sm:flex-1 sm:py-6"
-                          )
-                        }
-                      >
-                        {({ active, checked }) => (
-                          <>
-                            <RadioGroup.Label as="span">
-                              {size.name}
-                            </RadioGroup.Label>
-                            {size.inStock ? (
-                              <span
-                                className={classNames(
-                                  active ? "border" : "border-2",
-                                  checked
-                                    ? "border-indigo-500"
-                                    : "border-transparent",
-                                  "pointer-events-none absolute -inset-px rounded-md"
-                                )}
-                                aria-hidden="true"
-                              />
-                            ) : (
-                              <span
-                                aria-hidden="true"
-                                className="pointer-events-none absolute -inset-px rounded-md border-2 border-gray-200"
-                              >
-                                <svg
-                                  className="absolute inset-0 h-full w-full stroke-2 text-gray-200"
-                                  viewBox="0 0 100 100"
-                                  preserveAspectRatio="none"
-                                  stroke="currentColor"
-                                >
-                                  <line
-                                    x1={0}
-                                    y1={100}
-                                    x2={100}
-                                    y2={0}
-                                    vectorEffect="non-scaling-stroke"
-                                  />
-                                </svg>
-                              </span>
-                            )}
-                          </>
-                        )}
-                      </RadioGroup.Option>
-                    ))}
-                  </div>
-                </RadioGroup>
-              </div>
-
               <a
                 href="/login"
                 className="mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 py-3 px-8 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
               >
-                Add to bag
+                Adicionar ao carrinho
               </a>
             </form>
           </div>
 
           <div className="py-10 lg:col-span-2 lg:col-start-1 lg:border-r lg:border-gray-200 lg:pt-6 lg:pb-16 lg:pr-8">
-            {/* Description and details */}
             <div>
-              <h3 className="sr-only">Description</h3>
-
-              <div className="space-y-6">
-                <p className="text-base text-gray-900">{product.description}</p>
+              <div>
+                <h3 className="">s</h3>
+                <div className="space-y-6">
+                  <p className="text-base text-gray-900"></p>
+                </div>
               </div>
             </div>
 
@@ -343,14 +218,6 @@ export default function Cod() {
                     </li>
                   ))}
                 </ul>
-              </div>
-            </div>
-
-            <div className="mt-10">
-              <h2 className="text-sm font-medium text-gray-900">Details</h2>
-
-              <div className="mt-4 space-y-6">
-                <p className="text-sm text-gray-600">{product.details}</p>
               </div>
             </div>
           </div>
